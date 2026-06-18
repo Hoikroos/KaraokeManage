6b5bdd29e399ae00d05d3a389f3bc670362ecd16
Võ Đình Hội <hoivdpd10303@gmail.com>
2026-06-18 09:55:43 +0700

    anh nen

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -227,7 +227,7 @@ export default function Dashboard() {
       {/* Lớp nền mờ nghệ thuật cho Dashboard chính */}
       <div className="fixed inset-0 -z-10 overflow-hidden pointer-events-none">
         <img
-          src="https://images.unsplash.com/photo-1519751138004-ad0e0e90547b?q=80&w=2070&auto=format&fit=crop"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqILfHqShF9QmFdOyzl6OWk2UPrmanimj41WZ7iDc54w&s=10"
           alt="background"
           className="w-full h-full object-cover blur-[150px] opacity-[0.1] scale-110"
         />
